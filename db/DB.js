29a13203a3f1bbc5204de424cba6cb29d7340c0b
nethmiumaya <umayagk@gmail.com>
2024-08-02 08:24:01 +0530

    front end part in POS system

---
+++ db/DB.js
@@ -0,0 +1,32 @@
+export let Customers = [
+  {
+    custId: "C01",
+    custName: "Umaya",
+    custAddress: "Galle",
+    custSalary: "21",
+  },
+
+  {
+    custId: "C02",
+    custName: "Nethmi",
+    custAddress: "Galle",
+    custSalary: "30",
+  },
+];
+
+export let Items = [
+  {
+    itemId: "I01",
+    itemName: "Item1",
+    itemQty: 10,
+    itemPrice: 3,
+  },
+  {
+    itemId: "I02",
+    itemName: "Item2",
+    itemQty: 50,
+    itemPrice: 10,
+  },
+];
+
+export let Orders = [];
